a20f7a9247cd14870e9fc0a65a88f81fae7c7b14
Mark Hill <futonfutonda@gmail.com>
2016-02-04 10:58:07 +0000

    added hit points spec

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,3 +1,11 @@
 require_relative '../spec_helper.rb'
 
-feature 'Have hit points'
+feature 'Have hit points'do
+  scenario 'fill in names'do
+    visit('/')
+    fill_in :player_1_name, with: 'Dave'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+    expect(page).to have_content('Mittens: 60HP')
+  end
+end
