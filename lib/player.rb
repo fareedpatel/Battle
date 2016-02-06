1c059110b4249001329c682288e5b053dd235109
fareedpatel <fareed_patel@hotmail.com>
2016-02-06 17:59:59 +0000

    Challenge 14

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,23 @@
 class Player
-	attr_reader :name
+	attr_reader :name, :hit_points
 
-	def initialize(name)
+DEFAULT_HIT_POINTS= 60
+
+	def initialize(name, hit_points = DEFAULT_HIT_POINTS)
 		@name = name
+		@hit_points = hit_points
 	end
+
+	def attack(name)
+		name.got_a_smack
+	end
+
+	def got_a_smack
+		@hit_points -= 10
+	end
+
+
+
+	
 end
+
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,16 +1,24 @@
 require_relative '../spec_helper.rb'
 require 'capybara/dsl'
 require 'selenium-webdriver'
+require 'player'
 
 feature 'Attacking' do 
 before do
     include Capybara::DSL
     Capybara.default_driver = :selenium
  end
-scenario 'Player 1 attacks player 2'do
+	scenario 'Player 1 attacks player 2'do
     sign_in_and_play
     click_link 'Attack'
     expect(page).to have_content 'Dave attacked Fareed'
   end
 
+	scenario 'reduce player 2 HP by 10' do 
+			sign_in_and_play
+			click_link 'Attack'
+			click_link 'OK'
+			expect(page).not_to have_content "Fareed: 60HP"
+			expect(page).to have_content "Fareed: 50HP"
+	end
 end
