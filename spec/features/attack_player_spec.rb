e5a22a401f3039e9d5fbd09104d16d071c493f04
fareedpatel <fareed_patel@hotmail.co.uk>
2016-04-30 15:30:16 +0100

    wrote attack spec feature and test passes

---
+++ spec/features/attack_player_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../spec_helper.rb'
+
+feature 'Attacking' do
+	scenario 'attack player 2' do
+    sign_in_and_play
+    click_link('Attack')
+    expect(page).to have_content 'Dave attacked Mitten'
+  end
+end
