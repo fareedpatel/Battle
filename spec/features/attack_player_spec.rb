c43996195c01696a1337f71c422cd5a89e28a329
fareedpatel <fareed_patel@hotmail.co.uk>
2016-05-04 18:11:19 +0100

    implementing hit points

--- spec/features/attack_player_spec.rb
+++ spec/features/attack_player_spec.rb
@@ -6,4 +6,10 @@ feature 'Attacking' do
     click_link('Attack')
     expect(page).to have_content 'Dave attacked Mitten'
   end
+  scenario 'reduce player 1 by 10HP' do
+    sign_in_and_play
+    click_link('Attack')
+    expect(page).not_to have_content 'Mitten: 50 HP'
+    expect(page).to have_content 'Mitten: 50HP'
+  end
 end
