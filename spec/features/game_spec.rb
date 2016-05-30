de8b899eabd589423b95b5b54426290d3697e3ab
fareedpatel <fareed_patel@hotmail.co.uk>
2016-05-30 16:09:12 +0100

    As two players, we can now switch turns

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -1,14 +1,39 @@
 require_relative '../spec_helper.rb'
 
 describe Game do 
-	subject (:game) { described_class.new }
+	subject (:game) { described_class.new(player_1, player_2) }
   let(:player_1) { double :player }
   let(:player_2) { double :player }
 
-    describe '#attack' do
+  describe '#player_1' do
+    it 'retrieves the first player' do
+      expect(game.player_1).to eq player_1
+    end
+  end
+
+  describe '#player_2' do
+    it 'receives the second player' do
+      expect(game.player_2).to eq player_2
+    end
+  end
+
+  describe '#attack' do
     it 'damages the player' do
       expect(player_2).to receive(:receive_damage)
       game.attack(player_2)
+    end
+  end
+  
+ describe '#current_turn' do
+  it 'starts as player 1' do
+    expect(game.current_turn).to eq player_1
+  end
+end
+  
+  describe '#switching_turns' do
+    it 'switches the turn' do
+      game.switch_turns
+      expect(game.current_turn).to eq player_2
     end
   end
 end
