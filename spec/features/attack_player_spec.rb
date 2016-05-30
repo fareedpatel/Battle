8692c5bc4f1f1873fa2a79c8569570238adfb13a
fareedpatel <fareed_patel@hotmail.co.uk>
2016-05-30 17:44:53 +0100

    As Player 1, So I can lose a game of Battle, I want Player 2 to attack me, and I want to get a confirmation
    The Link on the confirmation page was also switched for a form containing a button, as clicking it now changes the `$game` state. It now uses POST to do this.

--- spec/features/attack_player_spec.rb
+++ spec/features/attack_player_spec.rb
@@ -6,10 +6,17 @@ feature 'Attacking' do
     click_link('Attack')
     expect(page).to have_content 'Dave attacked Mitten'
   end
+  scenario 'be attacked by player 2' do
+    sign_in_and_play
+    click_link('Attack')
+    click_button('OK')
+    click_link('Attack')
+    expect(page).to have_content 'Mitten attacked Dave'
+  end 
   scenario 'reduce player 1 by 10HP' do
     sign_in_and_play
     click_link('Attack')
-    expect(page).not_to have_content 'Mitten: 50 HP'
+    expect(page).not_to have_content 'Mitten: 60 HP'
     expect(page).to have_content 'Mitten: 50HP'
   end
 end
